c14a2972a296553d238584a3a532d3ef23e68e67
Alessio Carcione <alessiocarcione@BLU33336.local>
2024-04-16 18:40:33 +0200

    Add angular Material

--- frontend/eventhub/src/app/app.component.ts
+++ frontend/eventhub/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { MatSlideToggle, MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatSlideToggleModule,MatSlideToggle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = signal('eventhub');
 
--- frontend/eventhub/src/app/app.config.ts
+++ frontend/eventhub/src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, NgModule } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { MatSlideToggle, MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideAnimationsAsync()]
 };
+
