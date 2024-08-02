bdfbe3d5b884d98cdbf98d4ec7b3b4ebc89a607e
e3ctrain <c.traina@reply.it>
2024-08-02 16:33:07 +0200

    add initial load db auth

--- backend/eventshub-auth/src/main/java/it/reply/buins/eventshubauth/EventshubAuthApplication.java
+++ backend/eventshub-auth/src/main/java/it/reply/buins/eventshubauth/EventshubAuthApplication.java
@@ -1,5 +1,12 @@
 package it.reply.buins.eventshubauth;
 
+import it.reply.buins.eventshubauth.entities.UserEntity;
+import it.reply.buins.eventshubauth.exceptions.AuthException;
+import it.reply.buins.eventshubauth.models.SignUpRequest;
+import it.reply.buins.eventshubauth.repositories.UserRepository;
+import it.reply.buins.eventshubauth.services.AuthService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +17,19 @@ public class EventshubAuthApplication {
 		SpringApplication.run(EventshubAuthApplication.class, args);
 	}
 
+	@Autowired
+	private AuthService service;
+
+	@PostConstruct
+	public void initDb() throws AuthException {
+		// For testing purposes only
+		// Not for production
+
+		SignUpRequest user = new SignUpRequest();
+		user.setEmail("user@user.it");
+		user.setUsername("user");
+		user.setPassword("password");
+
+		service.save(user);
+	}
 }
