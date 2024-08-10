268fe7e2f2f574f5d4734fcabc9cba287d88b13d
Alessio Carcione <alessiocarcione@BLU33336.local>
2024-08-10 17:19:58 +0200

    distinzione eventi

--- frontend/eventhub/src/app/pages/event-list/event-list.component.ts
+++ frontend/eventhub/src/app/pages/event-list/event-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { Router, Routes } from '@angular/router';
+import { Router } from '@angular/router';
 import { UserState } from '../../../state/user-state/user.state';
 import { UserObj } from '../../models/user';
 import { SelectSnapshot } from '@ngxs-labs/select-snapshot';
@@ -16,71 +16,68 @@ import { DialogService } from '../../services/dialog.service';
 import { AddEventModalComponent } from '../../modal/add-event-modal/add-event-modal.component';
 import { AuthenticationService } from '../../services/authentication.service';
 import { UserAction } from '../../../state/user-state/user.action';
-
-
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-event-list',
   standalone: true,
   providers: [Store],
-  imports: [SharedModule,MatButtonModule,MatListModule],
+  imports: [SharedModule, MatButtonModule, MatListModule,MatTabsModule],
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.scss'
 })
 export class EventListComponent {
 
-  @SelectSnapshot(UserState.getUserData) public userData: UserObj | undefined; // ritorna il correlation id
-  @SelectSnapshot(EventState.getEventData) public eventDataList: EventObj[] | undefined; // ritorna il correlation id
-  @SelectSnapshot(EventState.getMyEventData) public eventMyDataList: EventObj[] | undefined; // ritorna il correlation id
-  
+  @SelectSnapshot(UserState.getUserData) public userData: UserObj | undefined;
+  @SelectSnapshot(EventState.getEventData) public eventDataList: EventObj[] | undefined;
+  @SelectSnapshot(EventState.getMyEventData) public eventMyDataList: EventObj[] | undefined;
+
   readonly dialog = inject(MatDialog);
 
-  constructor(private router: Router, private authService: AuthenticationService,private store: Store, private eventService: EventsService,private dialogService: DialogService){
+  constructor(private router: Router, private authService: AuthenticationService, private store: Store, private eventService: EventsService, private dialogService: DialogService) {}
 
-  }
-
-  ngOnInit(){
-    if(!this.authService.isLoggedIn()){
-      this.dialogService.openDialogMessage("Utente non loggato","Fai la login");
+  ngOnInit() {
+    if (!this.authService.isLoggedIn()) {
+      this.dialogService.openDialogMessage("Utente non loggato", "Fai la login");
       this.router.navigate(['/login-page']);
+    } else {
+      const user = this.authService.getUserLogged();
+      this.store.dispatch(new UserAction.SetUserData({ email: user.email, password: user.password, username: user.username }));
+      this.loadEvents();
     }
-    else{
-      let user = this.authService.getUserLogged();
-      this.store.dispatch(new UserAction.SetUserData({email: user.email, password :user.password, username :user.username}));
-      this.eventService.retrieveAllEvents().then(res => {
-        this.store.dispatch(new EventAction.SetEventDataList(res));
-      }); 
-      this.eventService.retrieveMyEvents().then(res => {
-        this.store.dispatch(new EventAction.SetMyAEventDataList(res));
-      }); 
-    }
-    console.log(this.store);
   }
 
-  reloadingMyEvents(){
+  loadEvents() {
+    this.eventService.retrieveAllEvents().then(res => {
+      this.store.dispatch(new EventAction.SetEventDataList(res));
+    });
+    this.eventService.retrieveMyEvents().then(res => {
+      this.store.dispatch(new EventAction.SetMyAEventDataList(res));
+    });
+  }
+
+  reloadingMyEvents() {
     this.eventService.retrieveMyEvents();
   }
 
-  reloadingEvents(){
+  reloadingEvents() {
     this.eventService.retrieveAllEvents();
   }
 
-  openDescription(description:string){
-    this.dialogService.openDialogMessage(description,"La descrizione dell' "+description+"");
+  openDescription(description: string) {
+    this.dialogService.openDialogMessage(description, "La descrizione dell' " + description);
   }
 
-  goToLoginPage(){
+  goToLoginPage() {
     this.authService.logout();
     this.router.navigate(['/login-page']);
   }
 
-  openModalAdd(){
-    this.dialog.open(AddEventModalComponent).afterClosed().subscribe(res =>{
-      if(res.success){
-        this.reloadingMyEvents();
-        this.eventService.retrieveAllEvents();
+  openModalAdd() {
+    this.dialog.open(AddEventModalComponent).afterClosed().subscribe(res => {
+      if (res.success) {
+        this.loadEvents();
       }
     });
   }
 }
-
