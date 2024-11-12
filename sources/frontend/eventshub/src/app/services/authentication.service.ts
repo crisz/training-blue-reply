348ed4e4670d9c18f1b69e6d105353eff97783c7
Alessio Carcione <alessiocarcione@BLU33336.local>
2024-11-12 14:20:28 +0100

    creazione nuovo cookie

--- sources/frontend/eventshub/src/app/services/authentication.service.ts
+++ sources/frontend/eventshub/src/app/services/authentication.service.ts
@@ -28,8 +28,18 @@ export class AuthenticationService {
   }
 
   setUserLogged(user : UserObj) {
-    // convertire in cookie con scadenza 1h
+    //memorizza utente nello storage
     sessionStorage.setItem('user',JSON.stringify(user));
+    // Genera un valore casuale
+    const randomValue = Math.random().toString(36).substring(2, 10); // stringa alfanumerica casuale
+    // Concatena l'id dell'utente con il valore casuale
+    const userToken = `${user.id}-${randomValue}`;
+    // Calcola la scadenza del cookie a 1 ora
+    const expirationTime = new Date();
+    expirationTime.setTime(expirationTime.getTime() + (1 * 60 * 60 * 1000)); // 1 ora in millisecondi
+
+    // Salva il token nel cookie
+    document.cookie = `userId=${userToken}; expires=${expirationTime.toUTCString()}; path=/; secure; samesite=strict`;
   }
 
   getUserLogged():UserObj{
@@ -38,7 +48,8 @@ export class AuthenticationService {
   }
 
   isLoggedIn(): boolean {
-    return !!sessionStorage.getItem('user');
+   // Verifica l’esistenza del cookie 'userId'
+    return document.cookie.split('; ').some(cookie => cookie.startsWith('userId='));
   }
 
   logout() {
